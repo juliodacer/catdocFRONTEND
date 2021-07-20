19c2e4fc1054571c8bf15572ff4a1baf99ed8d6d
Julio Davila <jylcoder@gmail.com>
2021-07-20 18:50:04 -0500

    commons pre project presentation

--- src/app/models/legajo.ts
+++ src/app/models/legajo.ts
@@ -5,7 +5,6 @@ import { Generic } from "./generic";
 export class Legajo implements Generic{
     id: number;
     nombre: string;
+    documentos: string;
     createAt: string;
-    docentes: Docente[] = [];
-    evaluaciones: Evaluacion[] = [];
 }
